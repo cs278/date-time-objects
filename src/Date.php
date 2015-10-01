195e65cd660b815a17ca77b8436a3e3000b8ea6e
Chris Smith <chris@cs278.org>
2015-10-01 22:01:05 +0100

    Add Date object and tests

---
+++ src/Date.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+ * This file is part of the Date Time Objects package.
+ *
+ * (c) Chris Smith <chris@cs278.org>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Cs278\DateTimeObjects;
+
+use Assert\Assertion;
+
+final class Date
+{
+    // Properties are used in comparisons, preserve the order of these.
+
+    /** @var int */
+    private $year;
+
+    /** @var int */
+    private $month;
+
+    /** @var int */
+    private $day;
+
+    /** @var int */
+    private $dayOfWeek;
+
+    /** @var int */
+    private $dayOfYear;
+
+    /**
+     * Constructor.
+     *
+     * @param int $year
+     * @param int $month
+     * @param int $day
+     */
+    public function __construct($year, $month, $day)
+    {
+        Assertion::integer($year);
+        Assertion::integer($month);
+        Assertion::integer($day);
+
+        Assertion::range($year, 0, 9999);
+        Assertion::range($month, 1, 12);
+        Assertion::range($day, 1, 31);
+
+        if (!checkdate($month, $day, $year)) {
+            throw new \InvalidArgumentException;
+        }
+
+        if ($year >= 0 && $year <= 1582) {
+            trigger_error('Date outside gregorian calendar range.', E_USER_NOTICE);
+        }
+
+        $this->year = $year;
+        $this->month = $month;
+        $this->day = $day;
+        $this->dayOfWeek = (int) $this->toDateTime()->format('N');
+        $this->dayOfYear = ((int) $this->toDateTime()->format('z')) + 1;
+    }
+
+    public static function createFromDateTime(\DateTimeInterface $dt)
+    {
+        return self::createFromString($dt->format('Y-m-d'));
+    }
+
+    public static function createFromString($iso8601str)
+    {
+        Assertion::regex($iso8601str, '{^[0-9]{1,4}-(?:0?[1-9]|1[0-2])-(?:0?[1-9]|[12][0-9]|3[01])$}');
+
+        // @todo Handle garbage
+        list($year, $month, $day) = explode('-', $iso8601str, 3);
+
+        return new self((int) $year, (int) $month, (int) $day);
+    }
+
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    public function getMonth()
+    {
+        return $this->month;
+    }
+
+    public function getDay()
+    {
+        return $this->day;
+    }
+
+    public function getDayOfWeek()
+    {
+        return $this->dayOfWeek;
+    }
+
+    public function getDayOfYear()
+    {
+        return $this->dayOfYear;
+    }
+
+    private function toDateTime()
+    {
+        // @todo Probably a bad idea, a day is actually a date period.
+        return new \DateTimeImmutable((string) $this, new \DateTimeZone('UTC'));
+    }
+
+    public function __toString()
+    {
+        return sprintf('%04u-%02u-%02u', $this->year, $this->month, $this->day);
+    }
+}
---
+++ tests/DateTest.php
@@ -0,0 +1,147 @@
+<?php
+
+/*
+ * This file is part of the Date Time Objects package.
+ *
+ * (c) Chris Smith <chris@cs278.org>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Cs278\DateTimeObjects;
+
+final class DateTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreateFromDateTime()
+    {
+        $dt = new \DateTime('2012-04-11 23:21:32.840028');
+        $expected = new Date(2012, 04, 11);
+
+        $this->assertEquals($expected, Date::createFromDateTime($dt));
+    }
+
+    /** @dataProvider dataCreateFromString */
+    public function testCreateFromString($expected, $input)
+    {
+        $this->assertEquals($expected, Date::createFromString($input));
+    }
+
+    public function dataCreateFromString()
+    {
+        return [
+            [new Date(2000, 1, 1), '2000-01-01'],
+            [new Date(2000, 1, 1), '2000-1-01'],
+            [new Date(2000, 1, 1), '2000-01-1'],
+            [new Date(2000, 1, 1), '2000-1-1'],
+            [new Date(1583, 1, 1), '1583-01-01'],
+            [new Date(9999, 1, 1), '9999-01-01'],
+            [new Date(2000, 1, 1), '2000-1-1'],
+            [new Date(2000, 1, 2), '2000-1-2'],
+            [new Date(2000, 1, 3), '2000-1-3'],
+            [new Date(2000, 1, 4), '2000-1-4'],
+            [new Date(2000, 1, 5), '2000-1-5'],
+            [new Date(2000, 1, 6), '2000-1-6'],
+            [new Date(2000, 1, 7), '2000-1-7'],
+            [new Date(2000, 1, 8), '2000-1-8'],
+            [new Date(2000, 1, 9), '2000-1-9'],
+            [new Date(2000, 1, 10), '2000-1-10'],
+            [new Date(2000, 1, 11), '2000-1-11'],
+            [new Date(2000, 1, 12), '2000-1-12'],
+            [new Date(2000, 1, 13), '2000-1-13'],
+            [new Date(2000, 1, 14), '2000-1-14'],
+            [new Date(2000, 1, 15), '2000-1-15'],
+            [new Date(2000, 1, 16), '2000-1-16'],
+            [new Date(2000, 1, 17), '2000-1-17'],
+            [new Date(2000, 1, 18), '2000-1-18'],
+            [new Date(2000, 1, 19), '2000-1-19'],
+            [new Date(2000, 1, 20), '2000-1-20'],
+            [new Date(2000, 1, 21), '2000-1-21'],
+            [new Date(2000, 1, 22), '2000-1-22'],
+            [new Date(2000, 1, 23), '2000-1-23'],
+            [new Date(2000, 1, 24), '2000-1-24'],
+            [new Date(2000, 1, 25), '2000-1-25'],
+            [new Date(2000, 1, 26), '2000-1-26'],
+            [new Date(2000, 1, 27), '2000-1-27'],
+            [new Date(2000, 1, 28), '2000-1-28'],
+            [new Date(2000, 1, 29), '2000-1-29'],
+            [new Date(2000, 1, 30), '2000-1-30'],
+            [new Date(2000, 1, 31), '2000-1-31'],
+            [new Date(2000, 2, 1), '2000-2-1'],
+            [new Date(2000, 3, 1), '2000-3-1'],
+            [new Date(2000, 4, 1), '2000-4-1'],
+            [new Date(2000, 5, 1), '2000-5-1'],
+            [new Date(2000, 6, 1), '2000-6-1'],
+            [new Date(2000, 7, 1), '2000-7-1'],
+            [new Date(2000, 8, 1), '2000-8-1'],
+            [new Date(2000, 9, 1), '2000-9-1'],
+            [new Date(2000, 10, 1), '2000-10-1'],
+            [new Date(2000, 11, 1), '2000-11-1'],
+            [new Date(2000, 12, 1), '2000-12-1'],
+        ];
+    }
+
+    public function testConstruct()
+    {
+        $date = new Date(1988, 12, 25);
+
+        $this->assertInstanceOf('Cs278\DateTimeObjects\Date', $date);
+
+        return $date;
+    }
+
+    /** @depends testConstruct */
+    public function testGetYear(Date $date)
+    {
+        $this->assertSame(1988, $date->getYear());
+    }
+
+    /** @depends testConstruct */
+    public function testGetMonth(Date $date)
+    {
+        $this->assertSame(12, $date->getMonth());
+    }
+
+    /** @depends testConstruct */
+    public function testGetDay(Date $date)
+    {
+        $this->assertSame(25, $date->getDay());
+    }
+
+    /** @depends testConstruct */
+    public function testGetDayOfWeek(Date $date)
+    {
+        $this->assertSame(WeekDay::SUN, $date->getDayOfWeek());
+    }
+
+    /** @depends testConstruct */
+    public function testGetDayOfYear(Date $date)
+    {
+        // 1988 was a leap year.
+        $this->assertSame(360, $date->getDayOfYear());
+    }
+
+    /** @depends testConstruct */
+    public function testToString(Date $date)
+    {
+        $this->assertSame('1988-12-25', (string) $date);
+    }
+
+    /** @dataProvider dataComparison */
+    public function testComparison($expected, Date $a, Date $b)
+    {
+        // $result = $a <=> $b;
+        $result = $a == $b ? 0 : ($a > $b ? 1 : -1);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function dataComparison()
+    {
+        return [
+            [1, new Date(1990, 4, 16), new Date(1990, 4, 15)],
+            [0, new Date(1990, 4, 15), new Date(1990, 4, 15)],
+            [-1, new Date(1990, 4, 14), new Date(1990, 4, 15)],
+        ];
+    }
+}
